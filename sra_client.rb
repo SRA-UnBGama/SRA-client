003a4d3ad7e934bbecc56118845ac64cdbc68c76
Matheus S. Pereira <matheussilva.fga@gmail.com>
2015-07-22 14:34:35 -0300

    Adding error handling

--- sra_client.rb
+++ sra_client.rb
@@ -8,35 +8,44 @@ WITHOUT_PLACE = "none"
 
 loop {
 
-  server = TCPSocket.open( IP, PORT ) # Connect to the server
+  begin
+    server = TCPSocket.open( IP, PORT ) # Connect to the server
+  rescue Errno::ECONNREFUSED
+    retry
+  end
+
 
 	# Receive the messenger of the server.
-  msg_server = server.recvfrom( 10000 )
+  begin
+    msg_server = server.recvfrom( 10000 )
+  rescue Errno::ECONNRESET
+    retry
+  end
 
   place = msg_server.first.chomp
 
   if place != WITHOUT_PLACE
-    File.open('.place_name', 'w') do |file|
+    File.open('.place_name.txt', 'w') do |file|
       file.puts place
     end
 
     server.puts ''
   else
 
-    if File.exist?('.place_name')
-      place_name = File.open( '.place_name', 'r' )
+    if File.exist?('.place_name.txt')
+      place_name = File.open( '.place_name.txt', 'r' )
 
       place_name.each_line do |line|
         server.puts line.chomp
       end
     else
-      File.new(".place_name", "w")
+      File.new(".place_name.txt", "w")
       server.puts ''
     end
 
   end
 
-  sleep(0.1)
+  sleep(0.01)
 
   server.close
 }
